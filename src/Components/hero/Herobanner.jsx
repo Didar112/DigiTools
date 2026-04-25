f3292ba3211588a4f36cc41870dadc49b12ba52c
Didar112 <didarulislam4546@gmail.com>
2026-04-25 20:11:23 +0600

    complete hero part

--- src/Components/hero/Herobanner.jsx
+++ src/Components/hero/Herobanner.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import bannerImg from "../../assets/products/banner.png"
 import grpIcon from "../../assets/products/Group.png"
+import { CiPlay1 } from "react-icons/ci";
+
 
 const Herobanner = () => {
     return (
@@ -16,12 +18,13 @@ const Herobanner = () => {
             <img src={grpIcon} alt="" />New: AI-Powered Tools Available</div>
       <h1 className="text-5xl font-extrabold text-[72px]">Supercharge Your Digital Workflow</h1>
       <p className="py-6 font-normal text-[#627382]">
-        Access premium AI tools, design assets, templates, and productivity
+        Access premium AI tools, design assets, templates, and productivity <br />
 software—all in one place. Start creating faster today.
 <br />
 Explore Products
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="mr-2 btn btn-primary rounded-full">Get Started</button>
+      <button className="btn btn-outline btn-primary rounded-full"><CiPlay1 /> Watch Demo</button>
     </div>
   </div>
 </div>
