684061f140f0f7a05e1be22e803a6e4e3fb01d40
Didar112 <didarulislam4546@gmail.com>
2026-04-26 03:54:52 +0600

    partially implemented products section

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,14 +1,15 @@
 import { react, use } from "react";
+import AllCard from "../Card/AllCard";
 
 
 const Products = ({prodData}) => {
 
    const data = use(prodData)
-   console.log(data)
+   
 
     return (
-        <div>
-            
+        <div className="container mx-auto border mt-40">
+            <AllCard data={data}></AllCard>
         </div>
     );
 };
